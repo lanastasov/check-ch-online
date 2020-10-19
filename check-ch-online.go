30b94829577142183824150e59b1a3e6e9741e19
lanastasov <lubomir.anastasov@gmail.com>
2020-10-19 13:04:01 +0300

    if any of the users is online open the site

--- check-ch-online.go
+++ check-ch-online.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 
 	"github.com/PuerkitoBio/goquery"
@@ -11,12 +12,9 @@ import (
 
 // CheckUserOnline - Request the HTML page aka  user profile
 // and if online load the site in the browser
-func CheckUserOnline() {
+func CheckUserOnline(user string) int {
 
-	// res, err := http.Get("https://www.chess.com/member/davit_tiraturyan")
-	// res, err := http.Get("https://www.chess.com/member/danielnaroditsky")
-
-	res, err := http.Get("https://www.chess.com/member/erichansen")
+	res, err := http.Get("https://www.chess.com/member/" + user)
 
 	if err != nil {
 		log.Fatal(err)
@@ -32,20 +30,28 @@ func CheckUserOnline() {
 		log.Fatal(err)
 	}
 
-	// Find the review items
+	// Find the user profile
 	doc.Find(".profile-info-item-value").Each(func(i int, s *goquery.Selection) {
-		// it is the second class that contains the info
+		// it is the second class that contains the info that's why we check i == 1
 		if i == 1 {
 			player := s.Text()
 			if player == "In Live" || player == "Online Now" {
 				fmt.Println("YES!!!")
 				url := "https://chess.com/live"
 				exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+				os.Exit(0)
 			}
 		}
 	})
+	return 0
 }
 
 func main() {
-	CheckUserOnline()
+
+	// if any of the users is online open the site, although
+	// the data in user's profile is not always consistent(currently online user is written as 1 hr ago)
+	user := []string{"wonderfultime", "erichansen", "danielnaroditsky", "davit_tiraturyan", "maghalashvili"}
+	for i := 0; i < len(user); i++ {
+		CheckUserOnline(user[i])
+	}
 }
